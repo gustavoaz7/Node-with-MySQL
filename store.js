69e64e9548ba832606d2fd28041a7d79a15c6669
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-10-08 19:08:51 -0300

    Login route

--- store.js
+++ store.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto')
 const knex = require('knex')(require('./knexfile'))
+
 module.exports = {
   saltHashPassword,
   createUser ({ username, password }) {
@@ -10,10 +11,22 @@ module.exports = {
       encrypted_password: hash,
       username
     })
+  },
+  authenticate ({ username, password }) {
+    console.log(`Authenticating user ${username}`)
+    return knex('user').where({ username })
+      .then(([user]) => {
+        if (!user) return { success: false }
+        const { hash } = saltHashPassword({
+          password,
+          salt: user.salt
+        })
+        return { success: hash === user.encrypted_password }
+      })
   }
 }
-function saltHashPassword (password) {
-  const salt = randomString()
+
+function saltHashPassword ({ password, salt = randomString() }) {
   const hash = crypto
     .createHmac('sha512', salt)
     .update(password)
@@ -22,6 +35,7 @@ function saltHashPassword (password) {
     hash: hash.digest('hex')
   }
 }
+
 function randomString () {
   return crypto.randomBytes(4).toString('hex')
 }
